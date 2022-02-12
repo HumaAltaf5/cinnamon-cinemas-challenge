97993b94eda602194e162b25aa32060991a863a2
HumaAltaf5 <huma.altaf@gmail.com>
2022-02-12 20:09:54 +0000

    Removing unwanted Array in testToAllocateAll15SeatsRandomly

--- src/test/java/com/huma/cinnamon/AllocateSeatsTest.java
+++ src/test/java/com/huma/cinnamon/AllocateSeatsTest.java
@@ -78,7 +78,6 @@ public class AllocateSeatsTest {
     @Test
     public void testToAllocateAll15SeatsRandomly() {
         CinnamonTheatre cinnamon = new CinnamonTheatre();
-        String[] arr = new String[]{"A1, A2, A3"};
         ArrayList<String> expectedList = new ArrayList(15);
         expectedList.add("A1, A2, A3, A4, A5, B1, B2, B3, B4, B5, C1, C2, C3, C4, C5");
         BookingManager manager = new BookingManager(cinnamon);
