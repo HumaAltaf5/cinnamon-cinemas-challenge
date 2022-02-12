80773d0abcd27d7c099f534e7a7b21345b308349
HumaAltaf5 <huma.altaf@gmail.com>
2022-02-12 18:06:52 +0000

    Adding test to check for total number of seats in the theatre.

--- src/main/java/com/huma/cinnamon/CinnamonTheatre.java
+++ src/main/java/com/huma/cinnamon/CinnamonTheatre.java
@@ -1,4 +1,9 @@
 package com.huma.cinnamon;
 
 public class CinnamonTheatre {
+    private int capacity = 15;
+
+    public int getSeats() {
+        return this.capacity;
+    }
 }
--- src/test/java/com/huma/cinnamon/AllocateSeatsTest.java
+++ src/test/java/com/huma/cinnamon/AllocateSeatsTest.java
@@ -1,4 +1,20 @@
 package com.huma.cinnamon;
 
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
 public class AllocateSeatsTest {
+    @Test
+
+    public void testToGetTotalCinemaSeats() {
+        //Arrange
+        int expected = 15;
+        CinnamonTheatre cinnamon = new CinnamonTheatre();
+        //Act
+        int actual = cinnamon.getSeats();
+        //Assert
+        assertEquals(actual, expected);
+    }
+
 }
